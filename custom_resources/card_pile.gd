ed9cf64c6a27792a334a6edb4e2db2ace37b1e05
Enzo Rodriguez <enzo.rod.er@gmail.com>
2025-08-11 23:09:00 -0400

    added stats and starting deck

--- custom_resources/card_pile.gd
+++ custom_resources/card_pile.gd
@@ -12,4 +12,20 @@ func draw_card() -> Card:
 	var card = cards.pop_front()
 	card_pile_size_changed.emit(cards.size())
 	return card
-# review this file
+
+func add_card(card: Card):
+	cards.append(card)
+	card_pile_size_changed.emit(cards.size())
+
+func shuffle() -> void:
+	cards.shuffle()
+
+func clear() -> void:
+	cards.clear()
+	card_pile_size_changed.emit(cards.size())
+
+func _to_string() -> String:
+	var _card_strings: PackedStringArray = []
+	for i in range(cards.size()):
+		_card_strings.append("%s: %s" % [i+1, cards[i].id])
+	return "\n".join(_card_strings)
